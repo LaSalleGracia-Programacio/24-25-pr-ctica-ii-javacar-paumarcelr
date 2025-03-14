152908badee3c235e8314e76beeadc2c334346a4
Marcel Roquet <17marcelroquet@gmail.com>
2025-03-14 13:07:37 +0100

    clase GestorLlogers i metode calcular ingresos totals

---
+++ src/main/java/org/JavaCar/GestorLlogers.java
@@ -0,0 +1,12 @@
+package org.JavaCar;
+
+import java.util.List;
+
+public class GestorLlogers {
+    static public  double calcularIngressosTotals (List<Vehicle>, int dies){
+
+
+        return 0;
+    }
+
+}
