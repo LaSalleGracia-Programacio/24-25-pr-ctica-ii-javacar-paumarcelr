d7bb9c62ef4a4dfa857750d76ac8dcfeecb85740
Pau <pau.sole@gracia.lasalle.cat>
2025-03-14 13:07:29 +0100

    Implement Llogable interface

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,6 +1,6 @@
 package org.JavaCar;
 
-public abstract class  Vehicle {
+public abstract class  Vehicle implements Llogable{
     protected String matricula;
     protected String marca;
     protected String model;
