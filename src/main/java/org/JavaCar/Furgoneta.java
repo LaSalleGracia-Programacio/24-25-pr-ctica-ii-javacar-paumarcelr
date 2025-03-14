27f16bd2254fd7468afcc52348b9261954b3ba29
Pau <pau.sole@gracia.lasalle.cat>
2025-03-14 13:50:24 +0100

    Add capacitatCarga and constructor

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -1,4 +1,10 @@
 package org.JavaCar;
 
 public class Furgoneta extends Vehicle{
+    private int capacitatCarga;
+
+    public Furgoneta(String matricula, String marca, String model, double preuBase, int capacitatCarga, Motor motor, Roda[] rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes);
+        this.capacitatCarga = capacitatCarga;
+    }
 }
