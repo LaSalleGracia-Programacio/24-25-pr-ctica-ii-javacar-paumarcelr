cfbd30536d965815fafa12118e2cc07c7b078599
Pau <pau.sole@gracia.lasalle.cat>
2025-03-14 13:36:54 +0100

    Create nombrePlaces and constructor

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -1,4 +1,10 @@
 package org.JavaCar;
 
 public class Cotxe extends Vehicle{
+    private int nombrePlaces;
+    public Cotxe(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes, int nombrePlaces) {
+        super(matricula, marca, model, preuBase, motor, rodes);
+        this.nombrePlaces = nombrePlaces;
+    }
+
 }
