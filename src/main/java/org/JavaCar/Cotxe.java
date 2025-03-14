0439cd7bfa26d5f9248200ac054423fd612890d7
Pau <pau.sole@gracia.lasalle.cat>
2025-03-14 13:42:57 +0100

    Change variables order in cosntructor

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -2,7 +2,7 @@ package org.JavaCar;
 
 public class Cotxe extends Vehicle{
     private int nombrePlaces;
-    public Cotxe(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes, int nombrePlaces) {
+    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, Motor motor, Roda[] rodes) {
         super(matricula, marca, model, preuBase, motor, rodes);
         this.nombrePlaces = nombrePlaces;
     }
