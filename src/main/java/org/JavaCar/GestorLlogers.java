b25bedfc78fca55d93c6a3e93d349299a47d1973
Marcel Roquet <17marcelroquet@gmail.com>
2025-03-17 12:09:13 +0100

    metode alquilar vehicle fet

--- src/main/java/org/JavaCar/GestorLlogers.java
+++ src/main/java/org/JavaCar/GestorLlogers.java
@@ -1,8 +1,11 @@
 package org.JavaCar;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 public class GestorLlogers {
+    static Scanner input = new Scanner(System.in);
     static public double calcularIngressosTotals(ArrayList<Vehicle> vehicles, int dies) {
         double preutotal = 0;
         for (int i = 0; i < vehicles.size(); i++) {
@@ -19,5 +22,41 @@ public class GestorLlogers {
                 flitrada.add(vehicles.get(i));
         }
         return vehicles;
+    }
+    public static void alquilarVehicle(List<Vehicle> vehiclesDisp, List<Vehicle> vAlquilats){
+        String s;
+        System.out.print("Introdueix la matricula del vehicle que vols alquilar: ");
+        String matricula = input.nextLine();
+
+        // buscar el cotxe a la llista de vehicles disponibles a partir de la matricula
+        for (int i = 0; i < vehiclesDisp.size(); i++) {
+            // si coincideix calculem el preu a partir dels dies
+            if (matricula.equals(vehiclesDisp.get(i).matricula)){
+
+                System.out.println("Quants dies el vols alquilar? ");
+                int dies = input.nextInt();
+                // mostrem el preu del cotxe que ha dit. Userfrendly mostrant marca i model
+                double preu = vehiclesDisp.get(i).calcularPreu(dies);
+                System.out.println("El preu final serà del "+ vehiclesDisp.get(i).getMarca()+" "+vehiclesDisp.get(i).getModel()+"és de :"+preu+"€");
+
+                //confirmar que el usuari vol alquilar el vehicle
+                do {
+                    System.out.print("Clica (c) per confirmar o (f) per cancelar");
+                    s = input.nextLine();
+                    if (!s.equalsIgnoreCase("S") || !s.equalsIgnoreCase("F")){
+                        System.out.println("Error, escriu (s) o (f)");
+                    }
+                } while (!s.equalsIgnoreCase("S") || !s.equalsIgnoreCase("F"));
+                // si confirma li mostrem missatge de confirmació, eliminem el cotxe de la llista de desponibles i l'agrfim a la llista d'alquilats
+                if (s.equalsIgnoreCase("S")){
+                    vAlquilats.add(vehiclesDisp.get(i));
+                    vehiclesDisp.remove(i);
+                    System.out.println("Cotxe alquilat correctament :)");
+                    break;
+                } else {
+                    System.out.println("Compra cancelada");
+                }
+            }
+        }
     }
 }
