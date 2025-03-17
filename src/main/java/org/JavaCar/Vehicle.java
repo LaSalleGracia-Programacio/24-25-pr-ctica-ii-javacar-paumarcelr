28de3ea822fac2a5a98df95c314c17379bbbe1c1
Pau <pau.sole@gracia.lasalle.cat>
2025-03-17 09:03:32 +0100

    add variables for ticket

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -7,6 +7,12 @@ public abstract class  Vehicle implements Llogable{
     protected Motor motor;
     protected Roda[] rodes;
 
+    protected boolean enchufable;
+    protected double autonomiaElectirc;
+    protected int anyMatriculacio;
+
+    protected EtiquetaAmbiental etiqueta;
+
     public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes) {
         this.matricula = matricula;
         this.marca = marca;
@@ -14,6 +20,10 @@ public abstract class  Vehicle implements Llogable{
         this.preuBase = preuBase;
         this.motor = motor;
         this.rodes = rodes;
+
+        enchufable = false;
+        autonomiaElectirc = 160;
+        anyMatriculacio = 1995;
     }
 
     public double calcularPreu(int dies) {
@@ -35,5 +45,34 @@ public abstract class  Vehicle implements Llogable{
     public double getPreuBase() {
         return preuBase;
     }
+
+    public EtiquetaAmbiental getEtiqueta() {
+        return etiqueta;
+    }
+
+    public boolean isEnchufable() {
+        return enchufable;
+    }
+
+    public void setEnchufable(boolean enchufable) {
+        this.enchufable = enchufable;
+    }
+
+    public double getAutonomiaElectirc() {
+        return autonomiaElectirc;
+    }
+
+    public void setAutonomiaElectirc(double autonomiaElectirc) {
+        this.autonomiaElectirc = autonomiaElectirc;
+    }
+
+    public int getAnyMatriculacio() {
+        return anyMatriculacio;
+    }
+
+    public void setAnyMatriculacio(int anyMatriculacio) {
+        this.anyMatriculacio = anyMatriculacio;
+    }
+
 
 }
