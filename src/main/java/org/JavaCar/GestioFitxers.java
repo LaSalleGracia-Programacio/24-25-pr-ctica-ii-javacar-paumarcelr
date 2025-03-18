919312aad89417ee70afd6ead640f5a20d986fcf
Pau <pau.sole@gracia.lasalle.cat>
2025-03-18 17:28:58 +0100

    Add escriureVehicle

--- src/main/java/org/JavaCar/GestioFitxers.java
+++ src/main/java/org/JavaCar/GestioFitxers.java
@@ -6,5 +6,20 @@ import java.util.*;
 public class GestioFitxers {
 
     static String nomFitxer = "vehicles.dat";
+    public static void escriureVehicle(Vehicle v, int dies) {
+        File file = new File(nomFitxer);
+        boolean append = file.exists();
+
+        try (FileOutputStream fos = new FileOutputStream(file, append);
+             ObjectOutputStream oos = append
+                     ? new AfegirFinalFitxer(fos)
+                     : new ObjectOutputStream(fos)) {
+            InfoAlquiler info = new InfoAlquiler(v, dies);
+            oos.writeObject(info);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
